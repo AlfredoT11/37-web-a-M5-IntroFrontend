68c2a83822e4370d6e2b22a9d7a701c8801bbbe9
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-12-12 19:53:36 -0600

    Se agregan las stats a la pantalla de detalles

--- 5.3.2_Pokedex/src/pages/DetallePokemon.jsx
+++ 5.3.2_Pokedex/src/pages/DetallePokemon.jsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
 import PokemonCardComponent from "../components/PokemonCardComponent";
+import { useState, useEffect } from "react";
 
 function DetallePokemon() {
+
+    const [detallePokemon, setDetallePokemon] = useState({});
 
     // useParams() es un hook que le permite al componente acceder a los parámetros de la url.
     // Estos parámetros se almacenan en un objeto literal.
@@ -12,10 +15,31 @@ function DetallePokemon() {
     */
     const params = useParams();
 
+    useEffect(() => {
+        // async-await
+    
+        async function consultarDetallesPokemon() {
+          try {
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}/`);
+            let responseJson = await response.json();
+            console.log(responseJson);
+            setDetallePokemon(responseJson);
+          } catch (e) {
+            console.log(`Ocurrió un error`);
+            console.log(e);
+          }
+        }
+    
+        consultarDetallesPokemon();
+      }, []);
+
     return (
         <>
             <h1>{params.nombrePokemon}</h1>
             <PokemonCardComponent nombre={params.nombrePokemon} id={params.id}></PokemonCardComponent>
+            <ul>
+                {detallePokemon?.stats.map((stat, index) => <li>{stat.stat.name} : {stat.base_stat}</li>)}
+            </ul>
         </>
     )
 }
